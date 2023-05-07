614c5295997e0b5b3c6b579eff3f8947e97cbd22
zhangzy56 <doxoops@163.com>
2023-05-07 23:28:53 +0800

    feat: 完善路由配置

--- router/index.js
+++ router/index.js
@@ -1,12 +1,40 @@
 import { RouterMount, createRouter } from 'uni-simple-router'
 
 // console.log('process.env', process.env)
+// $lockStatus: https://www.hhyang.cn/src/router/api/routerInsatll.html#lockstatus-v-1-5-4
 
 const router = createRouter({
   platform: process.env.VUE_APP_PLATFORM, // eg: mp-weixin
   applet: {
     animationDuration: 0 // 默认 300ms
   },
+  // 每次跳转失败时都会触发此方法并传递失败原因
+  routerErrorEach: ({ type, msg }) => {
+    switch (type) {
+      case 0: // 表示 next(false)
+        router.$lockStatus = false
+        break
+
+      case 3: // 表示在获取页面栈的时候，页面栈不够level获取  APP退出应用
+        // #ifdef APP-PLUS
+        router.$lockStatus = false
+
+        uni.showModal({
+          title: '提示',
+          content: '您确定要退出应用吗？',
+          success: res => {
+            if (res.confirm) plus.runtime.quit()
+          }
+        })
+        // #endif
+        break
+
+      default:
+        // do-nothing
+        break
+    }
+  },
+  // 设置 h5 输入非定义页面的url时 跳转到 404 页
   routes: [
     ...ROUTES,
     {
@@ -23,6 +51,14 @@ const router = createRouter({
 //全局路由前置守卫
 router.beforeEach((to, from, next) => {
   console.log('跳转开始')
+
+  // 权限控制登录
+  // if (to.meta && to.meta.auth && !store.getters.isLogin) {
+  //   store.dispatch('showAuthModal')
+  //   next(false)
+  //   return
+  // }
+
   next()
 })
 
